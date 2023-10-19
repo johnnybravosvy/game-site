4fcdec3c65faa6450a1ff1a98b5e41cf7eff9bdd
Chibuzor <ezeufohchibuzor@gmail.com>
2023-10-19 22:28:55 +0100

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 import App from "./App";
+import theme from "./theme";
 import "./index.css";
-
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const theme = extendTheme({ colors });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
